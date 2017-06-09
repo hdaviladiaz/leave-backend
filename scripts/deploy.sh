6af8aeff7171090bd8db6635b999418484a383f4
Xavier Naunay <xnaunay@thoughtworks.com>
2017-06-09 17:07:05 -0500

    [#0] with @hdavila, @smolina - force heroku to add git remote using ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 heroku maintenance:on --app "$1"
 git remote rm heroku
-heroku git:remote -a "$1"
+heroku git:remote -a --ssh-git "$1"
 vault auth -method=cert -client-cert=/home/go/vault-certs/gocd-agent.crt -client-key=/home/go/vault-certs/gocd-agent.key
 heroku config:set TOKEN_JIGSAW=$(vault read --field=value secret/jigsaw)
 git push -f heroku master
