b6a6642dece7a2e91409e7675fbcb365bf2da001
Xavier Naunay <xnaunay@thoughtworks.com>
2017-06-09 16:31:47 -0500

    [#0] with @hdavila - corrected jigsaw token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@ heroku maintenance:on --app "$1"
 git remote rm heroku
 heroku git:remote -a "$1"
 vault auth -method=cert -client-cert=/home/go/vault-certs/gocd-agent.crt -client-key=/home/go/vault-certs/gocd-agent.key
-heroku config:set TOKEN_JIGSAW = vault read --field=value secret/jigsaw
+heroku config:set TOKEN_JIGSAW=$(vault read --field=value secret/jigsaw)
 git push -f heroku master
 heroku maintenance:off --app "$1"
