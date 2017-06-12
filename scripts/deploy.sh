6db9f31079f9dea6b57b0e173657149369921592
Xavier Naunay <xnaunay@thoughtworks.com>
2017-06-12 14:33:31 -0500

    [#6] with @smolina - changed deployment script to use env variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-heroku maintenance:on --app "$1"
+heroku maintenance:on --app "$APP_BACKEND-$APP_ENV"
 git remote rm heroku
-heroku git:remote -a --ssh-git "$1"
+heroku git:remote -a --ssh-git "$APP_BACKEND-$APP_ENV"
 vault auth -method=cert -client-cert=/home/go/vault-certs/gocd-agent.crt -client-key=/home/go/vault-certs/gocd-agent.key
 heroku config:set TOKEN_JIGSAW=$(vault read --field=value secret/jigsaw)
 git push -f heroku master
-heroku maintenance:off --app "$1"
+heroku maintenance:off --app "$APP_BACKEND-$APP_ENV"
