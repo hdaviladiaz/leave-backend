218b07ab9b936dcdb1f3d619b79b086f4ea12869
Ernesto Medina <ernezto@gmail.com>
2017-06-15 17:26:56 -0500

    [N6] - fixs line in deploy script causing error when deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 heroku maintenance:on --app "$APP_BACKEND-$APP_ENV"
-git remote rm heroku
-heroku git:remote -a --ssh-git "$APP_BACKEND-$APP_ENV"
+heroku git:remote --remote heroku --ssh-git "$APP_BACKEND-$APP_ENV"
 vault auth -method=cert -client-cert=/home/go/vault-certs/gocd-agent.crt -client-key=/home/go/vault-certs/gocd-agent.key
 heroku config:set TOKEN_JIGSAW=$(vault read --field=value secret/jigsaw)
 git push -f heroku master
