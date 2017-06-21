255ec1e2d98182fc5ce02568e2b194cc10217a26
hector <hdavila@thoughtworks.com>
2017-06-21 15:59:12 -0500

    [#10] with @ymarin - including rol in token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User
-  attr_accessor :username, :email
+  attr_accessor :username, :email, :rol
 
   def initialize(email)
     @email = email
     @username = email.split('@')[0]
+    @rol = admin ? 'admin' : 'user'
+  end
+
+  def admin
+    admins = ADMIN_USERS.split(',')
+    admins.include?(@username)
   end
 end
--- spec/unit/models/token_spec.rb
+++ spec/unit/models/token_spec.rb
@@ -2,22 +2,23 @@ require 'rails_helper'
 require 'jwt'
 
 describe Token do
-    subject(:token) { Token.new }
-    let(:user) { User.new(Faker::Internet.email) }
-    let(:jwt_secret) { Faker::Crypto.sha256 }
-    let(:payload) { user.instance_values }
+  subject(:token) { Token.new }
+  let(:user) { User.new(Faker::Internet.email) }
+  let(:jwt_secret) { Faker::Crypto.sha256 }
+  let(:payload) { user.instance_values }
+  let(:rol) { '0' }
 
-    before(:each) { ENV['JWT_SECRET'] = jwt_secret }
+  before(:each) { ENV['JWT_SECRET'] = jwt_secret }
 
-    it 'should encode user' do
-        expected_encoded = JWT.encode(payload, jwt_secret, JWT_ALGORITHM)
-        encoded_token = token.encode(user)
-        expect(encoded_token).to eq(expected_encoded)
-    end
+  it 'should encode user' do
+    expected_encoded = JWT.encode(payload, jwt_secret, JWT_ALGORITHM) + rol
+    encoded_token = token.encode(user)
+    expect(encoded_token).to eq(expected_encoded)
+  end
 
-    it 'should decode token' do
-        provided_token = JWT.encode(payload, jwt_secret, JWT_ALGORITHM)
-        decoded_user = token.decode(provided_token)
-        expect(decoded_user).to have_attributes(payload)
-    end
+  it 'should decode token' do
+    provided_token = JWT.encode(payload, jwt_secret, JWT_ALGORITHM) + rol
+    decoded_user = token.decode(provided_token)
+    expect(decoded_user).to have_attributes(payload)
+  end
 end
