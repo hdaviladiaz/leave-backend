57f9effba50bff6dc1cef76a065de4c093bb3c56
Carlos Ortiz <cortiz@thoughtworks.com>
2017-06-27 14:10:57 -0500

    [#26] with @dchushig - Add final tests to manager class to load initial data

--- app/controllers/manager_initial_data_controller.rb
+++ app/controllers/manager_initial_data_controller.rb
@@ -1,8 +1,46 @@
 class ManagerInitialDataController < ApplicationController
 
-  def get_total_leaves
-    leaves = [1]
-    return leaves
+  def initialize
+    @matrix_employee_information_data
   end
 
+  def get_initial_data
+    initial_data_manager = InitialDataManager. new
+    matrix_employee_information_data = initial_data_manager.get_initial_data_from_file()
+    return matrix_employee_information_data
+  end
+
+  def parse_row_data_to_json(row)
+    dictionary_row = {'start_date' => build_start_date(row[2] + row[3]),
+                      'end_date' => build_final_date,
+                      'return_date' => build_final_date,
+                      'employee_id' => row[0],
+                      'initial_load' => true
+                    }
+    return dictionary_row
+  end
+
+  def build_start_date(taken_leave_days)
+    Date.today - taken_leave_days.to_i
+  end
+
+  def build_final_date()
+    Date.today
+  end
+
+  def save_unique_leave_request(leave_request_params)
+    leave_request = LeaveRequest.new(leave_request_params)
+    return leave_request.save
+  end
+
+  def save_matrix_initial_data()
+    @matrix_employee_information_data = get_initial_data
+    for i in 0..(@matrix_employee_information_data.length - 1)
+      row = @matrix_employee_information_data[i]
+      if row[2].to_i + row[3].to_i > 0
+        row_json = parse_row_data_to_json(row)
+        save_unique_leave_request(row_json)
+      end
+    end
+  end
 end
