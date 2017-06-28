c667a61e1d8f61b471d9c5c2c14ba3b91bd546c7
Schubert D. Rodríguez <scrodrig@thoughtworks.com>
2017-06-28 10:14:01 -0500

    [#30] with @evelasco - add a component to select who will approve new leave request

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :people, only: [:index]
   resources :calendars
   get '/people/me' => 'people#show'
+  get '/people/except_me' => 'people#index_except_me'
   post '/auth/saml/callback' => 'auth#saml_callback'
   
 end
